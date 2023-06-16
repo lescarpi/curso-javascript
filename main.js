a2cba6666c27dabdbfa8d3072010e4fae1c4ef1d
Leandro Scarpi <leandroscarpigranelli@gmail.com>
2023-06-16 19:26:54 -0300

    Adicionado codigo javascript para o funcionamento do midi

---
+++ main.js
@@ -0,0 +1,17 @@
+function tocaSomPom(idElemento) {
+    document.querySelector(idElemento).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+
+    let tecla = listaDeTeclas[i];
+    let instrumento = listaDeTeclas[i].classList[1];
+
+    let idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSomPom(idAudio);
+    };
+}
